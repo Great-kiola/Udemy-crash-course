e0e3a3ca2017b4605dcc743fb12dbb5dce04386f
Ola <hollabodey.kilaso@gmail.com>
2025-04-09 16:26:47 +0100

    integrated MongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 // const express = require("express");
 
 import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js";
+
+dotenv.config()
+
+// console.log(process.env.MONGO_URI)
 
 const app = express();
 
+app.get("/products", (req, res) => {
+    res.send("List of products");
+});
+
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 })
+
