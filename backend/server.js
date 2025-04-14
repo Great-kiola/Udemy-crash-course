a22c2060718d91eca6b93c568726dc5f9826f8de
Ola <hollabodey.kilaso@gmail.com>
2025-04-14 18:38:04 +0100

    feat: implement product creation endpoint with validation

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
+import Product from "./models/productModel.js"
 
 dotenv.config()
 
@@ -10,12 +11,35 @@ dotenv.config()
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    res.send("List of products");
-});
+// app.get("/products", (req, res) => {
+
+    
+//     res.send("List of products");
+// });
+
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body;
+
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({sucess: false, message: "Please enter all the fields"})
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({sucess: true, data: newProduct})
+    } catch(error){
+        console.error("Error in creating product", error.message);
+        res.status(500).json({sucess: false, message: "Internal server error"})
+    }
+})
+
 
 app.listen(5000, () => {
     connectDB();
     console.log("Server started at http://localhost:5000");
 })
-
+ 
