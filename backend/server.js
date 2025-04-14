08bae56afa10c68d694b7fce3b5107a87ce07ba2
Ola <hollabodey.kilaso@gmail.com>
2025-04-14 23:11:03 +0100

    Completed Backend

--- backend/server.js
+++ backend/server.js
@@ -3,39 +3,17 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
-import Product from "./models/productModel.js"
+import productRoutes from "./routes/product.route.js"
 
 dotenv.config()
 
 // console.log(process.env.MONGO_URI)
 
 const app = express();
+app.use(express.json()) // to parse json data from the request body
 
-// app.get("/products", (req, res) => {
+app.use("/api/products", productRoutes)
 
-    
-//     res.send("List of products");
-// });
-
-
-app.post("/api/products", async (req, res) => {
-    const product = req.body;
-
-
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({sucess: false, message: "Please enter all the fields"})
-    }
-
-    const newProduct = new Product(product);
-
-    try{
-        await newProduct.save();
-        res.status(201).json({sucess: true, data: newProduct})
-    } catch(error){
-        console.error("Error in creating product", error.message);
-        res.status(500).json({sucess: false, message: "Internal server error"})
-    }
-})
 
 
 app.listen(5000, () => {
