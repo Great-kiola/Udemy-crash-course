25499d2f98971c80da3a9487654d8f80ff2e9337
Ola <hollabodey.kilaso@gmail.com>
2025-04-18 20:39:02 +0100

    refactor: remove zustand React bindings and related files
    - Deleted zustand React type definitions and implementations from react.d.ts and react.js.
    - Removed shallow utility and its TypeScript definitions and implementations.
    - Cleaned up traditional store implementations and definitions.
    - Removed unused vanilla store files and definitions.
    - Updated package.json to reflect changes and added cross-env for environment management.
    - Removed zustand dependency from package-lock.json.

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js"
 
+import path from "path"
+
 dotenv.config()
 
 // console.log(process.env.MONGO_URI)
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -14,6 +17,13 @@ app.use(express.json()) // to parse json data from the request body
 
 app.use("/api/products", productRoutes)
 
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*" , (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 
 app.listen(PORT, () => {
